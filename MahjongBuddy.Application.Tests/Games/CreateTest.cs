c9d3ead5b84856c5899d90e2aa1cbcec21abd5a3
tonny.thiatmaja <tthiatma@gmail.com>
2020-03-31 22:27:26 -0700

    initial attempt to add unit test project

---
+++ MahjongBuddy.Application.Tests/Games/CreateTest.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using MahjongBuddy.Application.Games;
+using MahjongBuddy.Application.Interfaces;
+using MahjongBuddy.Core;
+using Moq;
+using System.Threading;
+using Xunit;
+
+namespace MahjongBuddy.Application.Tests.Games
+{
+    public class CreateTest: TestBase
+    {
+        private readonly IMapper _mapper;
+
+        public CreateTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfile()); });
+            _mapper = mockMapper.CreateMapper();
+        }
+
+        [Fact]
+        public void Should_Create_Game()
+        {
+            var userAccessor = new Mock<IUserAccessor>();
+            userAccessor.Setup(u => u.GetCurrentUserName()).Returns("mainplayer");
+
+            var context = GetDbContext();
+
+            context.Users.AddAsync(new AppUser
+            {
+                Id = "a",
+                Email = "mainplayer@gmail.com.com",
+                UserName = "mainplayer"
+            });
+            context.SaveChangesAsync();
+
+            var gameCommand = new Create.Command
+            {
+                Title = "Game1"
+            };
+
+            var sut = new Create.Handler(context, userAccessor.Object);
+            var result = sut.Handle(gameCommand, CancellationToken.None).Result;
+
+            //Assert.Equal("Game1", context.Games);
+            //Assert.Equal(1, result.Attendees.Count);
+        }
+    }
+}
---
+++ MahjongBuddy.Application.Tests/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MahjongBuddy.EntityFramework.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace MahjongBuddy.Application.Tests
+{
+    public class TestBase
+    {
+        public MahjongBuddyDbContext GetDbContext(bool useSqlite = false)
+        {
+            var builder = new DbContextOptionsBuilder<MahjongBuddyDbContext>();
+            if (useSqlite)
+            {
+                builder.UseSqlite("Data Source=:memory", x => { });
+            }
+            else
+            {
+                builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            }
+
+            var dbContext = new MahjongBuddyDbContext(builder.Options);
+
+            if (useSqlite)
+            {
+                dbContext.Database.OpenConnection();
+            }
+
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+    }
+}
