4a075d53f30b7b1dd65816c4320b053b370b7d84
Tonny Thiatmaja <tthiatma@gmail.com>
2020-07-22 22:09:01 -0700

    fix user not forwarded on register, and disable some header policy

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -20,6 +20,9 @@ export default class UserStore {
     @action register = async (values: IUserFormValues) => {
       try{
         const user = await agent.User.register(values);
+        runInAction(() => {
+          this.user = user;
+        })
         this.rootStore.commonStore.setToken(user.token);
         this.rootStore.modalStore.closeModal();
         history.push("/games");
