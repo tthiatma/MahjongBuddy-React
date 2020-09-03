04c67e2f50d1c353818d5c9838e4f05121c3ca94
Tonny Thiatmaja <tthiatma@gmail.com>
2020-09-02 23:31:37 -0700

    update seed data with photo and more check when throwing tile

--- MahjongBuddy.EntityFramework/Migrations/SeedData/DefaultUserBuilder.cs
+++ MahjongBuddy.EntityFramework/Migrations/SeedData/DefaultUserBuilder.cs
@@ -26,8 +26,15 @@ namespace MahjongBuddy.EntityFramework.Migrations.SeedData
                     Id = "a",
                     DisplayName = "Tonny",
                     UserName = "Tonny",
-                    Email = "tonny@gmail.com"
+                    Email = "tonny@gmail.com" 
                 };
+                mainPlayer.Photos.Add(new Photo
+                {
+                    Id = "ps9p70gkirosjaudenvl",
+                    Url = "https://res.cloudinary.com/mahjongbuddy/image/upload/v1598673581/ps9p70gkirosjaudenvl.jpg",
+                    IsMain = true
+                });
+
                 var rightPlayer = new AppUser
                 {
                     Id = "b",
@@ -35,6 +42,13 @@ namespace MahjongBuddy.EntityFramework.Migrations.SeedData
                     UserName = "Mei",
                     Email = "mei@gmail.com"
                 };
+                rightPlayer.Photos.Add(new Photo
+                {
+                    Id = "akrhqqhnjsrsnycub48x",
+                    Url = "https://res.cloudinary.com/mahjongbuddy/image/upload/v1598673644/akrhqqhnjsrsnycub48x.jpg",
+                    IsMain = true
+                });
+
                 var topPlayer = new AppUser
                 {
                     Id = "c",
@@ -42,13 +56,26 @@ namespace MahjongBuddy.EntityFramework.Migrations.SeedData
                     UserName = "Peter",
                     Email = "peter@gmail.com"
                 };
+                topPlayer.Photos.Add(new Photo
+                {
+                    Id = "qzqes8q87s5q0acs0tos",
+                    Url = "https://res.cloudinary.com/mahjongbuddy/image/upload/v1598673707/qzqes8q87s5q0acs0tos.jpg",
+                    IsMain = true
+                });
+
                 var leftPlayer = new AppUser
                 {
                     Id = "d",
                     DisplayName = "Jason",
                     UserName = "Jason",
-                    Email = "jason@gmail.com"
+                    Email = "jason@gmail.com",
                 };
+                leftPlayer.Photos.Add(new Photo
+                {
+                    Id = "u6st5f6gbr9enrxbeeqa",
+                    Url = "https://res.cloudinary.com/mahjongbuddy/image/upload/v1598673746/u6st5f6gbr9enrxbeeqa.jpg",
+                    IsMain = true
+                });
 
                 var users = new List<AppUser>
                 {
